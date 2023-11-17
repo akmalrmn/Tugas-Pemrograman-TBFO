d1f274fe7eb2444a7af46e32469ebfac0ccd46f6
Mohammad Akmal Ramadan <118426250+akmalrmn@users.noreply.github.com>
2023-11-17 19:10:51 +0700

    feat: update parser no content included

--- parserOneString.py
+++ parserOneString.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 html = open('abc.html', 'r').read()
 
 soup = BeautifulSoup(html, 'html.parser')
+
+for tag in soup.find_all():
+    if tag.string:
+        tag.string = ''
+
 output = str(soup)
 output = ''.join(output.split())
 
