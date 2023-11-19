e4df2caac4f898957ea297b2b1400084e6b1b0e4
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2023-11-19 09:26:50 +0700

    Fix: Html to string

--- parserOneString.py
+++ parserOneString.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
 
-html = open('abc.html', 'r').read()
+def HTMLToString(file_path):
+    html = open(file_path, 'r').read()
 
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-for tag in soup.find_all():
-    if tag.string:
-        tag.string = ''
+    for tag in soup.find_all():
+        if tag.contents:
+            for content in tag.contents:
+                if isinstance(content, str):
+                    content.replace_with('')
 
-output = str(soup)
-output = ''.join(output.split())
+    output = str(soup)
 
-print(output)
+    return output
+
+print(HTMLToString("a.html"))
