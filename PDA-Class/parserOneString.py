02c1f6ed472d0216a04b3c41a4e4411a5df12b6e
Mohammad Akmal Ramadan <118426250+akmalrmn@users.noreply.github.com>
2023-11-23 13:52:07 +0700

    feat: last parser revision (emg iyh?)

--- PDA-Class/parserOneString.py
+++ PDA-Class/parserOneString.py
@@ -5,8 +5,12 @@ def HTMLToString(filepath):
         html_content = file.read()
 
     parser = html.HTMLParser(remove_blank_text=True)
-    tree = html.fragments_fromstring(html_content, parser=parser)
 
-    output = ''.join(html.tostring(e, method='html').decode().replace('\n', '') for e in tree)
+    if '<html' in html_content and '<head' in html_content:
+        tree = html.fromstring(html_content, parser=parser)
+        output = html.tostring(tree, method='xml').decode().replace('\n', '')
+    else:
+        tree = html.fragments_fromstring(html_content, parser=parser)
+        output = ''.join(html.tostring(e, method='xml').decode().replace('\n', '') for e in tree)
 
     return output
