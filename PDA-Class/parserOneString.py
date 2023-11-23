b24094c00b2f84c9555a78c739c22f67a10f773b
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2023-11-23 16:24:32 +0700

    Update parserOneString.py

--- PDA-Class/parserOneString.py
+++ PDA-Class/parserOneString.py
@@ -14,3 +14,24 @@ def HTMLToString(filepath):
         output = ''.join(html.tostring(e, method='xml').decode().replace('\n', '') for e in tree)
 
     return output
+
+def HTMLToString(file_path):
+    result = ""
+    inTag = False
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+        for char in content:
+            if char == '<':
+                result += char
+                inTag = True
+            elif char == '>':
+                result += char
+                inTag = False
+            elif not inTag and char in [' ', '\t', '\n']:
+                continue
+            else:
+                result += char
+
+    return result
